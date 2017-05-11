3aff9f91cfd863fa179f73f6a547c12f41cea93d
= <webdev.jameslemire@gmail.com>
2017-05-11 16:14:11 -0600

    JL solution - :construction:

--- src/components/EditToggle.js
+++ src/components/EditToggle.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 export default class EditToggle extends Component {
   render() {
     return (
-      <div>
-        <select className="dropDownContainer ml0" onChange={ (e) => this.props.update(e.target.value) }>
-          <option value="true"> Allow Edit </option>
-          <option value="false"> Disable Edit </option>
-        </select>
-      </div>
+      <select className="dropDownContainer ml0" onChange={ (e) => this.props.update(e.target.value) }>
+        <option value="true"> Allow Edit </option>
+        <option value="false"> Disable Edit </option>
+      </select>
     )
   }
 }
