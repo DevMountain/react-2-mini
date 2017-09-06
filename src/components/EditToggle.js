c835b2afe383545c1f9c70bcdbb2986983b94217
Landon <landonwjohnson@gmail.com>
2017-09-06 08:53:25 -0600

    Finished the theme changer

--- src/components/EditToggle.js
+++ src/components/EditToggle.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class EditToggle extends Component {
   render() {
     return (
-      <select className="dropDownContainer ml0">
+      <select className="dropDownContainer ml0" onChange={ (e) => this.props.update(e.target.value) }>
         <option value="true"> Allow Edit </option>
         <option value="false"> Disable Edit </option>
       </select>
--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -9,13 +9,18 @@ export default class SizeChanger extends Component {
   }
 
   // componentWillReceiveProps
+  componentWillReceiveProps(props){
+    this.setState({ allowEdit: props.allowEdit });
+  }
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e) => this.props.update( parseInt(e.target.value) ) } disabled={ this.state.allowEdit === "false"}>
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
+        <option value="15"> 15 </option>
+        <option value="16"> 16 </option>
       </select>
     )
   }
